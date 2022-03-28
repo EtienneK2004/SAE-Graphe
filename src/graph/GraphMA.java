09a7d7833316d0e93a8c140c129bf21a7202d330
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2022-03-28 23:48:45 +0200

    Ajout de l'attribut matriceAdj, des prototypes demandés et du constructeur.

--- src/graph/GraphMA.java
+++ src/graph/GraphMA.java
@@ -1,5 +1,30 @@
 package graph;
 
-public class GraphMA {
+import java.util.Arrays;
 
+public class GraphMA {
+	
+	private boolean[][] matriceAdj;
+	
+	public GraphMA(int nbNoeuds) {
+		this.matriceAdj = new boolean[nbNoeuds][nbNoeuds];
+		for(boolean[] ligne : matriceAdj)
+			Arrays.fill(ligne, false);
+	}
+	
+	public void ajouterArc(int entrant, int sortant) {
+		
+	}
+	
+	public boolean aArc(int entrant, int sortant) {
+		return false;
+	}
+	
+	public int dOut(int entrant) {
+		return 0;
+	}
+	
+	public int dIn(int sortant) {
+		return 0;
+	}
 }
