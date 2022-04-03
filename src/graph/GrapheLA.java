d7087c47f018fb8c56ab82546e0e57879331cafb
BadiiiX <59808543+BadiiiiX@users.noreply.github.com>
2022-04-03 13:43:59 +0200

    Fin du projet
    + Graph LA
    + Documentation
    
    Tests fonctionnel :+1:

--- src/graph/GrapheLA.java
+++ src/graph/GrapheLA.java
@@ -3,35 +3,110 @@ package graph;
 import java.util.ArrayList;
 
 public class GrapheLA {
-	private ArrayList<Integer> listeAdj;
-	private int nbNoeuds;
-	
-	public GrapheLA(int nbNoeuds) {
-		this.nbNoeuds = nbNoeuds;
-		listeAdj = new ArrayList<>();
-	}
-	
-	public int getNbNoeuds() {
-		return nbNoeuds;
-	}
-	
-	public void ajouterArc(int entrant, int sortant) {
-	}
-	
-	public boolean aArc(int entrant, int sortant) {
-		return false;
-	}
-	
-	public int dOut(int entrant) {
-		return 0;
-	}
-	
-	public int dIn(int sortant) {
-		return 0;
-	}
-	
-	@Override
-	public String toString() {
-		return super.toString();
-	}
+    private final ArrayList<Integer>[] listeAdj;
+    private int nbNoeuds;
+
+    public GrapheLA(int nbNoeuds) {
+        this.nbNoeuds = nbNoeuds;
+        this.listeAdj = new ArrayList[nbNoeuds];
+
+        for (int i = 0; i < nbNoeuds; i++) {
+            this.listeAdj[i] = new ArrayList<>();
+        }
+    }
+
+    /**
+     * Vérifie si le noeud est dans le graphe
+     *
+     * @param arc Arc à vérifier
+     * @return true si le noeud est dans le graphe, false sinon
+     */
+    private boolean checkArc(int arc) {
+        return arc - 1 < this.nbNoeuds && arc - 1 >= 0;
+    }
+
+    /**
+     * Retourne le nombre de noeuds du graphe
+     *
+     * @return nombre de noeuds du graphe
+     */
+    public int getNbNoeuds() {
+        return nbNoeuds;
+    }
+
+    /**
+     * Ajoute un arc entre deux noeuds
+     *
+     * @param entrant Noeud d'entrée
+     * @param sortant Noeud de sortie
+     */
+    public void ajouterArc(int entrant, int sortant) {
+        if (!(checkArc(entrant) && checkArc(sortant))) return;
+
+        this.listeAdj[entrant - 1].add(sortant);
+    }
+
+    /**
+     * Retourne si l'arc entre deux noeuds est présent dans le graphe
+     *
+     * @param entrant Noeud d'entrée
+     * @param sortant Noeud de sortie
+     * @return true si l'arc entre deux noeuds est présent dans le graphe, false sinon
+     */
+    public boolean aArc(int entrant, int sortant) {
+        if (!(checkArc(entrant) && checkArc(sortant))) return false;
+
+        return this.listeAdj[entrant - 1].contains(sortant);
+    }
+
+    /**
+     * Retourne le nombre de successeurs d'un noeud
+     *
+     * @param entrant Noeud d'entrée
+     * @return Nombre de successeurs d'un noeud
+     */
+    public int dOut(int entrant) {
+        if (!checkArc(entrant)) return 0;
+
+        return this.listeAdj[entrant - 1].size();
+    }
+
+    /**
+     * Retourne le nombre de prédécesseurs d'un noeud
+     *
+     * @param sortant Noeud de sortie
+     * @return Nombre de prédécesseurs d'un noeud
+     */
+    public int dIn(int sortant) {
+        if (!checkArc(sortant)) return 0;
+
+        int nbArcs = 0;
+        for (int i = 0; i < this.nbNoeuds; i++) {
+            if (this.listeAdj[i].contains(sortant)) {
+                nbArcs++;
+            }
+        }
+
+        return nbArcs;
+    }
+
+    /**
+     * Retourne la liste d'adjacence du graphe sous chaine de caractères
+     *
+     * @return Liste d'adjacence du graphe
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < this.nbNoeuds; i++) {
+            sb.append(i + 1).append(" -> ");
+            for (int value : this.listeAdj[i]) {
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
 }
--- src/graph/GrapheMA.java
+++ src/graph/GrapheMA.java
@@ -3,57 +3,86 @@ package graph;
 import java.util.Arrays;
 
 public class GrapheMA {
-	
-	private boolean[][] matriceAdj;
-	
-	public GrapheMA(int nbNoeuds) {
-		this.matriceAdj = new boolean[nbNoeuds][nbNoeuds];
-		for(boolean[] ligne : matriceAdj)
-			Arrays.fill(ligne, false);
-	}
-	
-	public int getNbNoeuds() {
-		return matriceAdj.length;
-	}
-	
-	public void ajouterArc(int entrant, int sortant) {
-		matriceAdj[entrant-1][sortant-1] = true;
-	}
-	
-	public boolean aArc(int entrant, int sortant) {
-		return matriceAdj[entrant-1][sortant-1];
-	}
-	
-	public int dOut(int entrant) {
-		int cpt = 0;
-		for(boolean arc : matriceAdj[entrant-1])
-			if(arc)
-				cpt++;
-		return cpt;
-	}
-	
-	public int dIn(int sortant) {
-		int cpt = 0;
-		for(int i = 0; i < getNbNoeuds(); i++)
-			if(matriceAdj[i][sortant-1])
-				cpt++;
-		return cpt;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for(boolean[] ligne : matriceAdj) {
-			for(boolean arc : ligne) {
-				if(arc) 
-					sb.append("1 ");
-				else
-					sb.append("0 ");
-			}
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
+
+    private final boolean[][] matriceAdj;
+
+    public GrapheMA(int nbNoeuds) {
+        this.matriceAdj = new boolean[nbNoeuds][nbNoeuds];
+        for (boolean[] ligne : matriceAdj)
+            Arrays.fill(ligne, false);
+    }
+
+    /**
+     * Retourne le nombre de noeuds du graphe
+     * @return le nombre de noeuds du graphe
+     */
+    public int getNbNoeuds() {
+        return matriceAdj.length;
+    }
+
+    /**
+     * Ajoute un arc entre les noeuds entrant et sortant
+     * @param entrant le noeud entrant
+     * @param sortant le noeud sortant
+     */
+    public void ajouterArc(int entrant, int sortant) {
+        matriceAdj[entrant - 1][sortant - 1] = true;
+    }
+
+    /**
+     * Retourne si l'arc entrant-sortant existe
+     * @param entrant le noeud entrant
+     * @param sortant le noeud sortant
+     * @return
+     */
+    public boolean aArc(int entrant, int sortant) {
+        return matriceAdj[entrant - 1][sortant - 1];
+    }
+
+    /**
+     * Retourne le nombre de successeurs d'un noeud
+     * @param entrant le noeud entrant
+     * @return le nombre de successeurs d'un noeud
+     */
+    public int dOut(int entrant) {
+        int cpt = 0;
+
+        for (boolean arc : matriceAdj[entrant - 1])
+            if (arc) cpt++;
+
+        return cpt;
+    }
+
+    /**
+     * Retourne le nombre de prédécesseurs d'un noeud
+     * @param sortant le noeud sortant
+     * @return le nombre de prédécesseurs d'un noeud
+     */
+    public int dIn(int sortant) {
+        int cpt = 0;
+
+        for (int i = 0; i < getNbNoeuds(); i++)
+            if (matriceAdj[i][sortant - 1]) cpt++;
+
+        return cpt;
+    }
+
+    /**
+     * Retourne la matrice d'adjacence du graphe sous forme de chaîne de caractères
+     * @return Matrice d'adjacence du graphe
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (boolean[] ligne : matriceAdj) {
+            for (boolean arc : ligne)
+                sb.append(arc ? "1 " : "0 ");
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
 }
 
 
